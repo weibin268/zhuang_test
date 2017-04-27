00ae4aad5d29b44a0de4f46289486a90afb50a57
weibin268 <binweizhuang@hotmail.com>
2017-04-27 15:58:03 +0800

    aaa

--- file_test.py
+++ file_test.py
@@ -1,13 +1,16 @@
-with open("./temp_input.txt", 'r',encoding='utf-8') as file:
-    content=file.read()
-    
-    lines = content.split('\n')
 
-    str="select '"+r"' union all select '".join(lines)+"'"
 
-    print(str)
+with open('./temp_input.txt', 'r', encoding='utf-8') as file:
+
+    CON = file.read()
+
+    LINS = CON.split('\n')
+
+    STR = "select '" + r"' union all select '".join(LINS) + "'"
+
+    print(STR)
 
     #print(content)
 
-with open('./temp_output.txt','w',encoding='utf-8') as file:
-    file.write(str)
+with open('./temp_output.txt', 'w', encoding='utf-8') as file:
+    file.write(STR)
